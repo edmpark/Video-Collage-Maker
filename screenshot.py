54654c6fd9702a3c089c4309542b318c088e684f
Edmond P <edmondpark1@gmail.com>
2020-10-25 18:20:53 -0700

    montage in correct order now + updated comments

--- screenshot.py
+++ screenshot.py
@@ -11,7 +11,7 @@ import time
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", required= True,
     help="path of video")
-ap.add_argument("-i", "--images", required=True,
+ap.add_argument("-i", "--images", default="data",
 	help="path to input directory of images")
 ap.add_argument("-s", "--split", type=int, default="4",
 	help="number of images to get from video for collage")
@@ -30,9 +30,6 @@ if not cam.isOpened():
     print ("could not open :")
     quit()
 
-# number of times to screenshot video in equal proportion of video length
-split = args["split"]
-
 try:
 
     # creating a folder named data
@@ -43,21 +40,30 @@ try:
 except OSError:
     print('Error: Creating directory of data')
 
-# frame
+# current frame # in loop
 currentframe = 0
 
-# gets duration of video
+# number of times to screenshot video in equal proportion of video length
+split = args["split"]
+
+# gets # of frames of split video segment 
 length = round(int(cam.get(cv2.CAP_PROP_FRAME_COUNT)) / split)
+
+#keeps track of how many screenshots were taken from video so far in loop
+image_count = 0
+
+#images array which will then all be combined after while loop
+images = []
 
 while (True): 
     # reading from frame
     ret, frame = cam.read()
-
     if ret:
-        if currentframe % length == 0:  
+        if currentframe % length == 0 and image_count < split:  
+            image_count += 1
             name = './data/frame' + str(currentframe) + '.jpg'
-            print('Creating...' + name)
             cv2.imwrite(name, frame)
+            images.append( cv2.imread(name) )
         currentframe += 1
     else:
       print("Done")
@@ -66,17 +72,6 @@ while (True):
 # Release all space and windows once done
 cam.release()
 cv2.destroyAllWindows()
-
-#------combining image part------
-# grab the paths to the images
-imagePaths = list(paths.list_images(args["images"]))
-images = []
-
-# loop over the list of image paths
-for imagePath in imagePaths:
-	# load the image and update the list of images
-	image = cv2.imread(imagePath)
-	images.append(image)
 
 # construct the montages for the images
 montages = build_montages(images, (196, 128), (args["col"], args["row"]))
@@ -87,5 +82,5 @@ for montage in montages:
 #removes all individual collage images in image directory
 files = glob.glob(args["images"]+"/*")
 for f in files:
-    os.remove(f)
+   os.remove(f)
 
